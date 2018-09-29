6f57680674e841f6164fe8a3c4003d4c9280e46b
michaelmathew <michael.mathew@stonybrook.edu>
2018-09-29 12:24:18 -0400

    base

---
+++ super_canvasser/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Voltech SuperCanvasser</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      <td>
+      <img width="75" src="Iconarchive-Blue-Election-Election-Campaign-02-Outline.svg" alt="Horn"></img>/>
+      </td>
+
+            <input id ="txtEmail" type="email" placeholder="Email"/>
+            <input id ="txtPassword" type="password" placeholder="Password"/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+/*
+<input placeholder="Username"/>
+       <input placeholder="Password"/>
+       <input type="submit" value="Submit"/>
+*/
