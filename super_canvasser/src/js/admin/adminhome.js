814d25f6ff11deb1bcbf51496940756fb57d1c3a
dsli <ds.li@outlook.com>
2018-10-14 16:01:07 -0400

    Adding basework for table and add/edit form

--- super_canvasser/src/js/admin/adminhome.js
+++ super_canvasser/src/js/admin/adminhome.js
@@ -18,3 +18,69 @@ import ReactTable from "react-table";
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
+  function logout() {
+    window.location.href = "http://localhost:3000";
+  };
+
+  function AddUser() {
+
+  }
+  function editUser() {
+
+  }
+
+  function render() {
+    const data = [];
+  
+    const columns = [{
+      Header: 'User ID',
+      accessor: 'id' // String-based value accessors!
+    }, {
+      Header: 'Name',
+      accessor: 'name',
+      //Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'User Type', // Required because our accessor is not a string
+      Header: 'type',
+      //accessor: d => d.friend.name // Custom value accessors!
+    }, { // How to put in the edit and delete buttons?
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
+  }
+
+  function Admin(props) {
+    const { classes } = props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <AccountCircle />
+            </IconButton>
+            <Typography color="inherit" className={classes.grow}>
+              Manager
+            </Typography>
+            <Button color="inherit">Campaigns</Button>
+            
+            <Button onClick={logout} color="inherit">Log out</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+  
