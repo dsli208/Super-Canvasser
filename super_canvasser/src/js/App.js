e386312b01af1c3c426bb1dac9d4d6380a43b324
michaelmathew <michael.mathew@stonybrook.edu>
2018-10-13 23:48:39 -0400

    Manager Page + Logout in localhost3000

--- super_canvasser/src/js/App.js
+++ super_canvasser/src/js/App.js
@@ -2,41 +2,23 @@ import React, { Component } from 'react';
 import Image from 'react-image-resizer';
 import '../css/App.css';
 import appLogo from '../img/app_logo.png';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import {AccountCircle, VpnKey, Email} from '@material-ui/icons';
-//import {Tabs, Tab} from 'react-bootstrap';
 import LoginAndRegister from './LoginAndRegister';
 import {BrowserRouter, Route } from 'react-router-dom';
-
-
-
-
-
-
-
-
+import Manager from './manager/Manager';
+import Main from './Main';
 
 class App extends Component {
    
    render() {
       return (
-         <Grid className="container" container spacing={0} direction='column' justify='center'>
-            <Grid item xs={12} className="appLogo" container justify='center'>
-               <Image src={appLogo} height={150} width={150}/>
-            </Grid>
-            <Grid item xs={12} className="header" container justify='center'>
-               <h1 className='title'>Super Canvasser</h1>
-            </Grid>
+         <BrowserRouter>
+            <div>
+               <Route path='/' exact strict component={Main}/>
+               <Route path='/manager' exact strict component={Manager}/>
+            </div>
+         </BrowserRouter>
 
-            <BrowserRouter>
-               <Route path='/' component={LoginAndRegister}/>
-            </BrowserRouter>
-
-
-
-         </Grid>
       );
    }
 }
