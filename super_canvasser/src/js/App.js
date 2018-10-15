4b020f13626684126b02209fe4377d04cd565572
dsli <ds.li@outlook.com>
2018-10-15 04:17:01 -0400

    Add app.js

--- super_canvasser/src/js/App.js
+++ super_canvasser/src/js/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { Component } from 'react';
 import '../css/App.css';
 import {BrowserRouter, Route } from 'react-router-dom';
@@ -29,35 +29,3 @@ class App extends Component {
 }
 
 export default App;
-=======
-import React, { Component } from 'react';
-import '../css/App.css';
-import {BrowserRouter, Route } from 'react-router-dom';
-import Manager from './manager/Manager';
-import ManagerCampaignsList from './manager/ManagerCampaignsList';
-import ManagerCanvassersList from './manager/ManagerCanvassersList';
-import ManagerLocationsList from './manager/ManagerLocationsList';
-import Canvasser from './canvasser/Canvasser';
-import Main from './Main';
-
-class App extends Component {
-   
-   render() {
-      return (
-         <BrowserRouter>
-            <div>
-               <Route path='/' exact strict component={Main}/>
-               <Route path='/manager' exact strict component={Manager}/>
-               <Route path='/manager/campaigns' exact strict component={ManagerCampaignsList}/>
-               <Route path='/manager/canvassers' exact strict component={ManagerCanvassersList}/>
-               <Route path='/manager/locations' exact strict component={ManagerLocationsList}/>
-               <Route path='/canvasser' exact strict component={Canvasser}/>
-            </div>
-         </BrowserRouter>
-
-      );
-   }
-}
-
-export default App;
->>>>>>> 2d7a37dd457c63b69b11493a09bffa378cbf5056
