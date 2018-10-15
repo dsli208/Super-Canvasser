54133c6c4f413c8a3939e01be03b11ffa4bcdfc7
dsli <ds.li@outlook.com>
2018-10-15 04:07:26 -0400

    Remove extraneous conflict markers

--- super_canvasser/src/js/Main.js
+++ super_canvasser/src/js/Main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { Component } from 'react';
 import Image from 'react-image-resizer';
 import '../css/App.css';
@@ -28,35 +28,4 @@ class Main extends Component {
    }
 }
 
-=======
-import React, { Component } from 'react';
-import Image from 'react-image-resizer';
-import '../css/App.css';
-import appLogo from '../img/app_logo.png';
-import Grid from '@material-ui/core/Grid';
-import LoginAndRegister from './LoginAndRegister';
-import {BrowserRouter, Route } from 'react-router-dom';
-
-class Main extends Component {
-   
-   render() {
-      return (
-         <Grid className="container" container spacing={0} direction='column' justify='center'>
-            <Grid item xs={12} className="appLogo" container justify='center'>
-               <Image src={appLogo} height={150} width={150}/>
-            </Grid>
-            <Grid item xs={12} className="header" container justify='center'>
-               <h1 className='title'>Super Canvasser</h1>
-            </Grid>
-
-            <BrowserRouter>
-               <Route path='/' exact strict component={LoginAndRegister}/>
-            </BrowserRouter>
-
-         </Grid>
-      );
-   }
-}
-
->>>>>>> 2d7a37dd457c63b69b11493a09bffa378cbf5056
 export default Main;
