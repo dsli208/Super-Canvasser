71142ff6e8c835750a6c259e419a8ef9bd9e62c7
michaelmathew <michael.mathew@stonybrook.edu>
2018-11-04 13:48:23 -0500

    Right now trying to make list of locations to reorder

--- super_canvasser/src/js/canvasser/CanvasserViewMap.js
+++ super_canvasser/src/js/canvasser/CanvasserViewMap.js
@@ -11,6 +11,7 @@ import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
 import TextField from '@material-ui/core/TextField';
 
 
+
 const style = {
   backgroundColor: '#ffffff',
   position: 'absolute',
@@ -24,7 +25,119 @@ const field_style = {
 };
 
 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
 class CanvasserViewMap extends React.Component {
+
+
+
+
+  myFunction= () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap',
+    });
+    map.addListener('zoom_changed', () => {
+      this.setState({
+        zoom: map.getZoom(),
+      });
+    });
+  
+    map.addListener('maptypeid_changed', () => {
+      this.setState({
+        maptype: map.getMapTypeId(),
+      });
+    });
+    let marker = new window.google.maps.Marker({
+      map: map,
+      position: {lat: -33.8688, lng: 151.2195},
+    });
+  
+    // initialize the autocomplete functionality using the #pac-input input box
+    let inputNode = document.getElementById('pac-input');
+    map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
+    let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
+  
+    autoComplete.addListener('place_changed', () => {
+      let place = autoComplete.getPlace();
+      let location = place.geometry.location;
+  
+      this.setState({
+        place_formatted: place.formatted_address,
+        place_id: place.place_id,
+        place_location: location.toString(),
+      });
+  
+      // bring the selected place in view on the map
+      map.fitBounds(place.geometry.viewport);
+      map.setCenter(location);
+  
+      marker.setPlace({
+        placeId: place.place_id,
+        location: location,
+      });
+    });
+  }
+
+
+
+
+
+  sortList = () => {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("id01");
+    switching = true;
+    while (switching) {
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      for (i = 0; i < (b.length - 1); i++) {
+        shouldSwitch = false;
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark the switch as done:*/
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div style={style}>
@@ -53,6 +166,21 @@ class CanvasserViewMap extends React.Component {
                 <Grid item xs={12} >
                   <div className='manager-map'>
                     <br/> <h1>Map</h1> <br/>
+
+
+              
+
+
+                    <Button onClick={this.myFunction} variant="outlined"> Next Location </Button>           
+                    <Button  onclick={this.sortList} variant="outlined"> Re-order Visits </Button>           
+                    <ul id="id01">
+  <li>Oslo</li>
+  <li>Stockholm</li>
+  <li>Helsinki</li>
+  <li>Berlin</li>
+  <li>Rome</li>
+  <li>Madrid</li>
+</ul>
                     <MyMap/>
                   </div>
                 </Grid>
